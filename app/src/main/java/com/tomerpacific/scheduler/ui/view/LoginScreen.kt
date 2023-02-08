5d2e41d370f1183c0145bc55158e18ad3e68ffd5
unknown <tomerpacific@gmail.com>
2023-02-08 11:51:00 +0200

    Scheduler ui and logic for password field

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/LoginScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/LoginScreen.kt
@@ -1,18 +1,19 @@
 package com.tomerpacific.scheduler.ui.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,6 +22,9 @@ import androidx.compose.ui.unit.sp
 fun LoginScreen() {
 
     var username by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordVisible by remember { mutableStateOf(false) }
+
 
     Column(modifier = Modifier.fillMaxSize()) {
         Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
@@ -36,6 +40,27 @@ fun LoginScreen() {
             onValueChange = { username = it },
             label = { Text("Enter your username") },
             leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "userIcon", tint = Color.Blue)},
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Enter your password") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "userIcon", tint = Color.Blue)},
+            trailingIcon = {
+                val iconImage = if (passwordVisible)  {
+                    Icons.Default.Visibility
+                } else  {
+                    Icons.Default.VisibilityOff
+                }
+
+                IconButton(onClick = {
+                    passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = iconImage, contentDescription = "passwordIcon", tint = Color.Blue)
+                }
+               },
         )
 
 
