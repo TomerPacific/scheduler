86732c12422bdb7154b98f0a1feb730111ba4706
unknown <tomerpacific@gmail.com>
2023-02-08 10:45:30 +0200

    Scheduler creating login screen with title and text field for username

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/LoginScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/LoginScreen.kt
@@ -1,4 +1,43 @@
 package com.tomerpacific.scheduler.ui.view
 
-class LoginScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen() {
+
+    var username by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Text(text = "Please Login",
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold)
+        }
+        Spacer(modifier = Modifier.padding(horizontal = 0.dp, vertical = 50.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Enter your username") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "userIcon", tint = Color.Blue)},
+        )
+
+
+    }
 }
