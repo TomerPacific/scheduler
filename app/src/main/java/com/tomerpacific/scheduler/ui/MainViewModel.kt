026499ce3ef4fe8820cf6e17b6b286657831921c
unknown <tomerpacific@gmail.com>
2023-02-09 21:05:13 +0200

    Scheduler creating view model

---
+++ app/src/main/java/com/tomerpacific/scheduler/ui/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.tomerpacific.scheduler.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    init {
+
+    }
+
+}
