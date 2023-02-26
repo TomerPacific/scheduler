4ad5612a34f71b6b35ac4b6f61cf239e778ff038
unknown <tomerpacific@gmail.com>
2023-02-26 22:25:40 +0200

    Scheduler styling text fields in login screen

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/LoginScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/LoginScreen.kt
@@ -1,11 +1,9 @@
 package com.tomerpacific.scheduler.ui.view
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
@@ -15,14 +13,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tomerpacific.scheduler.ui.model.MainViewModel
 
 @Composable
@@ -32,49 +27,69 @@ fun LoginScreen(viewModel: MainViewModel) {
     var password by remember { mutableStateOf(TextFieldValue("")) }
     var passwordVisible by remember { mutableStateOf(false) }
 
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top) {
-        Row {
-            Text("Welcome", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row() {
+            Text(
+                "Welcome To Scheduler",
+                style = MaterialTheme.typography.h1
+            )
         }
-        Row(modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Center) {
-            Text(text = "To",
-                fontWeight = FontWeight.Bold,
-                fontSize = 25.sp)
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.width(300.dp).padding(top = 300.dp),
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Enter your username") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "userIcon",
+                        tint = Color.Blue
+                    )
+                },
+                shape = RoundedCornerShape(20.dp)
+            )
+            OutlinedTextField(
+                modifier = Modifier.width(300.dp),
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Enter your password") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                shape = RoundedCornerShape(20.dp),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "userIcon",
+                        tint = Color.Blue
+                    )
+                },
+                trailingIcon = {
+                    val iconImage = if (passwordVisible) {
+                        Icons.Default.Visibility
+                    } else {
+                        Icons.Default.VisibilityOff
+                    }
+
+                    IconButton(onClick = {
+                        passwordVisible = !passwordVisible
+                    }) {
+                        Icon(
+                            imageVector = iconImage,
+                            contentDescription = "passwordIcon",
+                            tint = Color.Blue
+                        )
+                    }
+                },
+            )
         }
-    }
-
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom) {
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Enter your username") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "userIcon", tint = Color.Blue) },
-        )
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Enter your password") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "userIcon", tint = Color.Blue) },
-            trailingIcon = {
-                val iconImage = if (passwordVisible)  {
-                    Icons.Default.Visibility
-                } else  {
-                    Icons.Default.VisibilityOff
-                }
-
-                IconButton(onClick = {
-                    passwordVisible = !passwordVisible }) {
-                    Icon(imageVector = iconImage, contentDescription = "passwordIcon", tint = Color.Blue)
-                }
-            },
-        )
     }
 }
