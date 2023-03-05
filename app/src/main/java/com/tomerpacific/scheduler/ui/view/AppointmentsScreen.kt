a64af012a457dcf33aad3d214b0c5759cd263682
unknown <tomerpacific@gmail.com>
2023-03-05 22:26:04 +0200

    Scheduler setting what UI to present based on login status

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentsScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentsScreen.kt
@@ -5,28 +5,31 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import com.tomerpacific.scheduler.ui.model.MainViewModel
 
 @Composable
 fun AppointmentsScreen(viewModel: MainViewModel, onUserLogout: () -> Unit) {
 
-    val isUserConnected by remember { mutableStateOf(viewModel.isUserConnected()) }
+    val user = viewModel.user.observeAsState()
 
     Column(modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center) {
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.End) {
-            TextButton(onClick = {
-                viewModel.logout()
-                onUserLogout()
-            }) {
-                Text("Logout")
+        if (user != null) {
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End) {
+                TextButton(onClick = {
+                    viewModel.logout()
+                    onUserLogout()
+                }) {
+                    Text("Logout")
+                }
             }
         }
         Row(modifier = Modifier.fillMaxSize(),
             horizontalArrangement = Arrangement.Center) {
-            if (!isUserConnected) {
+            if (user == null) {
                 CircularProgressIndicator()
             } else {
                 Text(text = "Welcome!")
