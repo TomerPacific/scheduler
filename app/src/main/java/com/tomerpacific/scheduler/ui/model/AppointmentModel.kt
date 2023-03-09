6e4a292a328c0b6d74b1cf14504b9ee59bcae7fa
unknown <tomerpacific@gmail.com>
2023-03-09 23:51:46 +0200

    Scheduler creating model for appointments

---
+++ app/src/main/java/com/tomerpacific/scheduler/ui/model/AppointmentModel.kt
@@ -0,0 +1,8 @@
+package com.tomerpacific.scheduler.ui.model
+
+data class AppointmentModel(
+    val appointmentDate: Long,
+    val appointmentPlace: String,
+    val appointmentDuration: String
+
+)
