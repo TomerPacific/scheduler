0ba9b9c4041202927ddedbfdce142325ac7f9da8
unknown <tomerpacific@gmail.com>
2023-03-12 23:01:42 +0200

    Scheduler styling add appointment screen

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/AddAppointmentScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/AddAppointmentScreen.kt
@@ -1,4 +1,25 @@
 package com.tomerpacific.scheduler.ui.view
 
-class AddAppointmentScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.tomerpacific.scheduler.ui.model.MainViewModel
+
+@Composable
+fun AddAppointmentScreen(viewModel: MainViewModel) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text("Add An Appointment",
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center)
+        }
+    }
 }
