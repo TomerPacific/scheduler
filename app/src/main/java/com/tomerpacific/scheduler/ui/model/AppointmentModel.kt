6390c8d0ebf15a3d95cde5dcd09907e2aeaaf147
unknown <tomerpacific@gmail.com>
2023-03-29 21:48:14 +0300

    Scheduler logic to generate a unique id for each appointment

--- app/src/main/java/com/tomerpacific/scheduler/ui/model/AppointmentModel.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/model/AppointmentModel.kt
@@ -1,8 +1,17 @@
 package com.tomerpacific.scheduler.ui.model
 
+import java.util.*
+
 data class AppointmentModel(
     val appointmentDate: Long,
     val appointmentPlace: String,
-    val appointmentDuration: String
+    val appointmentDuration: String,
+    val appointmentId: String = generateId()
+) {
+    companion object {
 
-)
+        private fun generateId(): String {
+            return UUID.randomUUID().toString()
+        }
+    }
+}
