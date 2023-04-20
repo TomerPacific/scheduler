2af87dc54995f8836b5653e3a0490f299a8685ab
unknown <tomerpacific@gmail.com>
2023-04-20 20:54:09 +0300

    Scheduler adding method to truncate timestamp by removing miliseconds

--- app/src/main/java/com/tomerpacific/scheduler/Utils.kt
+++ app/src/main/java/com/tomerpacific/scheduler/Utils.kt
@@ -1,11 +1,18 @@
 package com.tomerpacific.scheduler
 
 import java.util.*
+import kotlin.math.floor
 
 object Utils {
 
     fun convertTimestampToDate(timestamp: Long): Date {
         return Date(timestamp)
+    }
+
+    fun truncateTimestamp(timestamp: Long): Long {
+        val t = timestamp.toDouble() / 1000L
+        val floored = floor(t) * 1000L
+        return floored.toLong()
     }
 
 }
