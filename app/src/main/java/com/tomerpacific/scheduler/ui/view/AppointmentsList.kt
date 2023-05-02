27e41fb6262efea80bea8c97040e679a63c56c0b
unknown <tomerpacific@gmail.com>
2023-05-02 21:38:54 +0300

    Scheduler adding UI to show scheduled appointments properly

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentsList.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentsList.kt
@@ -1,14 +1,22 @@
 package com.tomerpacific.scheduler.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SupervisedUserCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tomerpacific.scheduler.Utils
 import com.tomerpacific.scheduler.ui.model.MainViewModel
 
@@ -19,10 +27,38 @@ fun AppointmentsList(viewModel: MainViewModel) {
 
     if (appointments.value != null &&
         appointments.value!!.isNotEmpty()) {
+        Row(modifier = Modifier.fillMaxWidth().padding(bottom = 25.dp),
+             verticalAlignment = Alignment.CenterVertically) {
+            Text("Your Scheduled Appointments",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+        }
         LazyColumn(modifier = Modifier.fillMaxSize()) {
             items(appointments.value!!) { appointment ->
-                Row() {
-                    Text(text = Utils.convertTimestampToDate(appointment.appointmentDate).toString())
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .padding(start = 5.dp, end = 5.dp, bottom = 5.dp),
+                    shape = MaterialTheme.shapes.small,
+                    elevation = 10.dp,
+                    border = BorderStroke(width = 3.dp, color = Color.Black)
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Icon(modifier = Modifier.padding(start = 5.dp),
+                            imageVector = Icons.Default.SupervisedUserCircle,
+                            contentDescription = "Cancel Icon")
+                        Text(text = Utils.convertTimestampToDate(appointment.appointmentDate).toString(),
+                            fontSize = 15.sp)
+                        TextButton(onClick = {
+
+                        },
+                            shape = RoundedCornerShape(50)
+                        ) {
+                            Text("Cancel", fontWeight = FontWeight.Bold)
+                        }
+                    }
                 }
             }
         }
