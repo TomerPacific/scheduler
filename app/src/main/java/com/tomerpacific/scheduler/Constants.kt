de7fb2b6902c27d743fc267c413b35e8f1134e4b
unknown <tomerpacific@gmail.com>
2023-05-11 22:35:45 +0300

    Scheduler creating constants class with constants for navigation

---
+++ app/src/main/java/com/tomerpacific/scheduler/Constants.kt
@@ -0,0 +1,5 @@
+package com.tomerpacific.scheduler
+
+const val NAVIGATION_DESTINATION_LOGIN = "login"
+const val NAVIGATION_DESTINATION_APPOINTMENTS = "appointments"
+const val NAVIGATION_DESTINATION_ADD_APPOINTMENT = "add-appointment"
