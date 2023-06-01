90c94b508c9ddc5c26009d3b8f4ebe129f8bc286
unknown <tomerpacific@gmail.com>
2023-06-01 23:42:26 +0300

    Scheduler adding text to splash screen

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/SplashScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/SplashScreen.kt
@@ -5,12 +5,16 @@ import android.os.Looper
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
@@ -41,14 +45,22 @@ fun SplashScreen(splashTimerEnd:() -> Unit) {
             .background(if (isSystemInDarkTheme()) Color.Black else Color.White)
             .fillMaxSize()
     ) {
-        Row(modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center) {
-            LottieAnimation(
-                composition = composition,
-                progress = { progress },
-            )
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center) {
+                Text("Scheduler", fontSize = 25.sp, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.h1)
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center) {
+                LottieAnimation(
+                    composition = composition,
+                    progress = { progress },
+                )
+            }
         }
+
 
     }
 }
