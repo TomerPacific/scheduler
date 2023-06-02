6099ddb5ca9b67efdada74411d31268eb22efc3d
unknown <tomerpacific@gmail.com>
2023-06-02 11:25:16 +0300

    Scheduler creating constants for navigation argument

--- app/src/main/java/com/tomerpacific/scheduler/Constants.kt
+++ app/src/main/java/com/tomerpacific/scheduler/Constants.kt
@@ -1,9 +1,18 @@
 package com.tomerpacific.scheduler
 
+
+/*
+* Navigation
+*/
 const val NAVIGATION_DESTINATION_SPLASH = "splash"
 const val NAVIGATION_DESTINATION_LOGIN = "login"
 const val NAVIGATION_DESTINATION_APPOINTMENTS = "appointments"
 const val NAVIGATION_DESTINATION_ADD_APPOINTMENT = "add-appointment"
 
+/*
+* Appointment Actions
+*/
 const val APPOINTMENT_ACTION_SCHEDULE = "schedule"
 const val APPOINTMENT_ACTION_CANCEL = "cancel"
+const val APPOINTMENT_ACTION_KET = "appointmentAction"
+const val ERROR_MESSAGE_KEY = "errorMsg"
