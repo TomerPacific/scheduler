562b43ae85d91cd6301d574447e39deca661e99e
unknown <tomerpacific@gmail.com>
2023-06-06 21:16:34 +0300

    Scheduler adding a callback when appointment is clicked

---
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentScreen.kt
@@ -0,0 +1,9 @@
+package com.tomerpacific.scheduler.ui.view
+
+import androidx.compose.runtime.Composable
+import com.tomerpacific.scheduler.ui.model.AppointmentModel
+
+@Composable
+fun AppointmentScreen(appointment: AppointmentModel) {
+
+}
