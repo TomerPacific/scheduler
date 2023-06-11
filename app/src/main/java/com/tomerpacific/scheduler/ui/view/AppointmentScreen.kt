79f9d0946eedca355afc8d9f4081dffa071d5a5e
unknown <tomerpacific@gmail.com>
2023-06-11 20:04:07 +0300

    Scheduler adding UI to present appointment details

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentScreen.kt
@@ -1,9 +1,39 @@
 package com.tomerpacific.scheduler.ui.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.tomerpacific.scheduler.Utils
 import com.tomerpacific.scheduler.ui.model.AppointmentModel
 
 @Composable
 fun AppointmentScreen(appointment: AppointmentModel) {
-
+    Column(modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Top) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                "Appointment Scheduled on : ${Utils.convertTimestampToDate(appointment.appointmentDate)}",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center)
+        }
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+            Text(
+                "Appointment Duration : ${appointment.appointmentDuration}",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center)
+        }
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+            Text(
+                "Appointment Place : ${appointment.appointmentPlace}",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center)
+        }
+    }
 }
