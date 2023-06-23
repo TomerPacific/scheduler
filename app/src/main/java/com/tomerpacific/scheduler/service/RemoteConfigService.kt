6ac348beddff8a44e5def8d6ffd61258224bb5a2
unknown <tomerpacific@gmail.com>
2023-06-23 11:12:36 +0300

    Scheduler creating class for remote config

---
+++ app/src/main/java/com/tomerpacific/scheduler/service/RemoteConfigService.kt
@@ -0,0 +1,34 @@
+package com.tomerpacific.scheduler.service
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.tomerpacific.scheduler.START_HOUR_FOR_APPOINTMENTS
+
+class RemoteConfigService {
+
+    private val remoteConfig = Firebase.remoteConfig
+
+    init {
+        val remoteConfigSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 2000
+        }
+
+        remoteConfig.setConfigSettingsAsync(remoteConfigSettings)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener { task ->
+
+            }.addOnFailureListener { error ->
+                print(error.localizedMessage)
+            }
+    }
+
+    fun getAppointmentStartTime(): Int {
+        return when(val appointmentStartTime = remoteConfig.getDouble("appointment_start_time")) {
+            0.0 -> START_HOUR_FOR_APPOINTMENTS
+            else -> appointmentStartTime.toInt()
+        }
+    }
+
+}
