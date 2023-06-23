d9bb6c4806369e3ef7f7c98bccfd9b78d405f6b3
unknown <tomerpacific@gmail.com>
2023-06-23 23:03:47 +0300

    Scheduler adding method to get end time of appointments

--- app/src/main/java/com/tomerpacific/scheduler/service/RemoteConfigService.kt
+++ app/src/main/java/com/tomerpacific/scheduler/service/RemoteConfigService.kt
@@ -3,6 +3,7 @@ package com.tomerpacific.scheduler.service
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.tomerpacific.scheduler.END_HOUR_FOR_APPOINTMENTS
 import com.tomerpacific.scheduler.R
 import com.tomerpacific.scheduler.START_HOUR_FOR_APPOINTMENTS
 
@@ -30,6 +31,13 @@ class RemoteConfigService {
         return when(val appointmentStartTime = remoteConfig.getDouble("appointment_start_time")) {
             0.0 -> START_HOUR_FOR_APPOINTMENTS
             else -> appointmentStartTime.toInt()
+        }
+    }
+
+    fun getAppointmentEndTime(): Int {
+        return when(val appointmentEndTime = remoteConfig.getDouble("appointment_end_time")) {
+            0.0 -> END_HOUR_FOR_APPOINTMENTS
+            else -> appointmentEndTime.toInt()
         }
     }
 
