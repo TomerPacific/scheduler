4c72e6448c5c4e2337816063e523a7e857afe5cc
unknown <tomerpacific@gmail.com>
2023-07-11 20:41:58 +0300

    Scheduler parsing configuration into model and refactoring logic accordingly

--- app/src/main/java/com/tomerpacific/scheduler/service/RemoteConfigService.kt
+++ app/src/main/java/com/tomerpacific/scheduler/service/RemoteConfigService.kt
@@ -4,10 +4,11 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.tomerpacific.scheduler.END_HOUR_FOR_APPOINTMENTS
+import com.tomerpacific.scheduler.FirebaseRemoteConfigurationsModel
 import com.tomerpacific.scheduler.R
 import com.tomerpacific.scheduler.REMOTE_CONFIG_APPOINTMENT_HOURS_KEY
 import com.tomerpacific.scheduler.START_HOUR_FOR_APPOINTMENTS
-import org.json.JSONObject
+import kotlinx.serialization.json.Json
 import java.time.LocalDateTime
 
 class RemoteConfigService {
@@ -24,7 +25,10 @@ class RemoteConfigService {
 
         remoteConfig.fetchAndActivate()
             .addOnCompleteListener { task ->
-
+                if (task.isSuccessful) {
+                    val remoteConfigValues = task.result
+                    print(remoteConfigValues)
+                }
             }.addOnFailureListener { error ->
                 print(error.localizedMessage)
             }
@@ -32,15 +36,18 @@ class RemoteConfigService {
 
     fun getAppointmentStartAndEndTimeByDay(currentDate: LocalDateTime): List<Int> {
         val appointmentStartAndEndTimesFromConfig = remoteConfig.getString(REMOTE_CONFIG_APPOINTMENT_HOURS_KEY)
-        val appointmentStartAndEndTimes = JSONObject(appointmentStartAndEndTimesFromConfig)
-        val currentDay = currentDate.dayOfWeek.toString()
+        val firebaseRemoteConfigurationsModel: FirebaseRemoteConfigurationsModel = Json.decodeFromString(
+            FirebaseRemoteConfigurationsModel.serializer(),
+            appointmentStartAndEndTimesFromConfig)
 
-        if (!appointmentStartAndEndTimes.has(currentDay)) {
-            return listOf(START_HOUR_FOR_APPOINTMENTS, END_HOUR_FOR_APPOINTMENTS)
+        return when (currentDate.dayOfWeek.toString()) {
+            "SUNDAY" -> firebaseRemoteConfigurationsModel.sunday
+            "MONDAY" -> firebaseRemoteConfigurationsModel.monday
+            "TUESDAY" -> firebaseRemoteConfigurationsModel.tuesday
+            "WEDNESDAY" -> firebaseRemoteConfigurationsModel.wednesday
+            "THURSDAY" -> firebaseRemoteConfigurationsModel.thursday
+            else -> listOf(START_HOUR_FOR_APPOINTMENTS, END_HOUR_FOR_APPOINTMENTS)
         }
-
-        val startAndEndTimesForToday = appointmentStartAndEndTimes.getJSONArray(currentDay)
-        return listOf(startAndEndTimesForToday.get(0) as Int, startAndEndTimesForToday.get(1) as Int)
     }
 
 
