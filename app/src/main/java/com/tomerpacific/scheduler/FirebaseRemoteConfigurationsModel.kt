658b4eb6a79b9f2e622e6e16b3277f55554c8911
unknown <tomerpacific@gmail.com>
2023-07-11 20:15:16 +0300

    Scheduler creating model class for remote configuration

---
+++ app/src/main/java/com/tomerpacific/scheduler/FirebaseRemoteConfigurationsModel.kt
@@ -0,0 +1,18 @@
+package com.tomerpacific.scheduler
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FirebaseRemoteConfigurationsModel(
+    @SerialName("SUNDAY")
+    val sunday: List<Int>,
+    @SerialName("MONDAY")
+    val monday: List<Int>,
+    @SerialName("TUESDAY")
+    val tuesday: List<Int>,
+    @SerialName("WEDNESDAY")
+    val wednesday: List<Int>,
+    @SerialName("THURSDAY")
+    val thursday: List<Int>
+)
