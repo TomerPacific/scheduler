65ad0cf77bc06fede07728587f62119fefd5fe3b
unknown <tomerpacific@gmail.com>
2023-08-01 20:29:58 +0300

    Scheduler adding constants for remote config day keys

--- app/src/main/java/com/tomerpacific/scheduler/Constants.kt
+++ app/src/main/java/com/tomerpacific/scheduler/Constants.kt
@@ -17,7 +17,18 @@ const val APPOINTMENT_ACTION_CANCEL = "cancel"
 const val APPOINTMENT_ACTION_KET = "appointmentAction"
 const val ERROR_MESSAGE_KEY = "errorMsg"
 
-const val REMOTE_CONFIG_APPOINTMENT_HOURS_KEY = "appointment_start_and_end_times_by_day"
-const val REMOTE_CONFIG_ADMIN_EMAIL_KEY = "admin_email"
 const val START_HOUR_FOR_APPOINTMENTS = 10
 const val END_HOUR_FOR_APPOINTMENTS = 19
+
+/*
+ * Remote Configuration Keys
+ */
+
+const val REMOTE_CONFIG_APPOINTMENT_HOURS_KEY = "appointment_start_and_end_times_by_day"
+const val REMOTE_CONFIG_ADMIN_EMAIL_KEY = "admin_email"
+const val REMOTE_CONFIG_SUNDAY_KEY = "SUNDAY"
+const val REMOTE_CONFIG_MONDAY_KEY = "MONDAY"
+const val REMOTE_CONFIG_TUESDAY_KEY = "TUESDAY"
+const val REMOTE_CONFIG_WEDNESDAY_KEY = "WEDNESDAY"
+const val REMOTE_CONFIG_THURSDAY_KEY = "THURSDAY"
+
