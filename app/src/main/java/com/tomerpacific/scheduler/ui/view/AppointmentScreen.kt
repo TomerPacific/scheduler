f65851414756e9c54824f4cc72e658ef06906ffd
unknown <tomerpacific@gmail.com>
2023-08-24 21:15:15 +0300

    Scheduler moving location permission logic to AppointmentScreen

--- app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentScreen.kt
+++ app/src/main/java/com/tomerpacific/scheduler/ui/view/AppointmentScreen.kt
@@ -1,5 +1,8 @@
 package com.tomerpacific.scheduler.ui.view
 
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.InlineTextContent
@@ -12,6 +15,8 @@ import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.HourglassFull
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,11 +30,26 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tomerpacific.scheduler.Utils
 import com.tomerpacific.scheduler.ui.model.AppointmentModel
+import com.tomerpacific.scheduler.ui.model.MainViewModel
 import kotlin.time.Duration
 
 @Composable
 fun AppointmentScreen(appointment: AppointmentModel,
                       onAddLocationPressed: () -> Unit) {
+
+    val areLocationPermissionsGranted = remember {
+        mutableStateOf(false)
+    }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isPermissionGranted ->
+            areLocationPermissionsGranted.value = isPermissionGranted
+            if (isPermissionGranted) {
+                onAddLocationPressed()
+            }
+        }
+    )
 
     val iconPlaceholderId = "iconPlaceholderId"
     val appointmentScheduledText: AnnotatedString = buildAnnotatedString {
@@ -45,7 +65,8 @@ fun AppointmentScreen(appointment: AppointmentModel,
                 Placeholder(
                     width = 20.sp,
                     height = 20.sp,
-                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center)
+                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center
+                )
             ) {
                 Icon(Icons.Filled.CalendarMonth, "Calendar icon", tint = Color.Black)
             }
@@ -65,7 +86,8 @@ fun AppointmentScreen(appointment: AppointmentModel,
                 Placeholder(
                     width = 20.sp,
                     height = 20.sp,
-                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center)
+                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center
+                )
             ) {
                 Icon(Icons.Filled.HourglassFull, "Hour glass icon", tint = Color.Black)
             }
@@ -85,30 +107,39 @@ fun AppointmentScreen(appointment: AppointmentModel,
                 Placeholder(
                     width = 20.sp,
                     height = 20.sp,
-                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center)
+                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center
+                )
             ) {
                 Icon(Icons.Filled.LocationOn, "Location icon", tint = Color.Black)
             }
         )
     )
 
-    Column(modifier = Modifier.fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Top) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 230.dp),
-            horizontalArrangement = Arrangement.Center) {
-            Text("Appointment Details",
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 230.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Appointment Details",
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center)
+                textAlign = TextAlign.Center
+            )
         }
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.White)
-            .padding(bottom = 20.dp),
-            horizontalArrangement = Arrangement.Center) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(bottom = 20.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
             Text(
                 appointmentScheduledText,
                 fontSize = 20.sp,
@@ -117,25 +148,34 @@ fun AppointmentScreen(appointment: AppointmentModel,
                 inlineContent = appointmentScheduledInlineText
             )
         }
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.White)
-            .padding(bottom = 20.dp),
-            horizontalArrangement = Arrangement.Center) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(bottom = 20.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
             Text(
                 appointmentDurationText,
                 fontSize = 20.sp,
                 textAlign = TextAlign.Center,
                 maxLines = 2,
-                inlineContent = appointmentDurationInlineText)
+                inlineContent = appointmentDurationInlineText
+            )
         }
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.White),
-            horizontalArrangement = Arrangement.Center) {
-            if (appointment.appointmentPlace.isNullOrEmpty()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            if (appointment.appointmentPlace.isEmpty()) {
                 Button(onClick = {
-                    onAddLocationPressed()
+                    if (areLocationPermissionsGranted.value) {
+                        onAddLocationPressed()
+                    } else {
+                        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    }
                 }) {
                     Text("Add A Location")
                 }
@@ -145,7 +185,8 @@ fun AppointmentScreen(appointment: AppointmentModel,
                     fontSize = 20.sp,
                     textAlign = TextAlign.Center,
                     maxLines = 2,
-                    inlineContent = appointmentPlaceInlineText)
+                    inlineContent = appointmentPlaceInlineText
+                )
             }
 
         }
